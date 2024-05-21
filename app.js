9880de038616e21aa61fd71cbbf8531d0cdaddf8
VladanS10 <vladan.nish@gmail.com>
2024-05-21 17:27:27 +0200

    comiting changes

--- app.js
+++ app.js
@@ -1,19 +1,84 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.status(200).json({
-        name: 'Vladan',
-        lastname: 'Stojic',
-        app: 'Natours',
-        message:'Hello from the server side!'
-    });
+app.use(express.json());
+
+// app.get('/', (req, res) =>{
+//     res
+//     .status(200)
+//     .json({
+//         name: 'Vladan',
+//         lastname: 'Stojic',
+//         app: 'Natours',
+//         message:'Hello from the server side!'
+//     });
+// });
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res
+    .status(200)
+    .json({
+        status: 'succes',
+        data: {
+            tours
+        }
+    })
 });
 
-app.post('/', (req, res) =>{
-    res.send('You can post to this endpoint...')
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+
+    // if(id > tours.length){
+    if(!tour){
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    
+    res
+    .status(200)
+    .json({
+        status: 'succes',
+        data: {
+            tour
+        }
+    })
 });
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id:newId}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, 
+    JSON.stringify(tours), 
+    err => {
+        res.status(201).json({
+            status: 'succes',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+
+});
+
+// app.post('/', (req, res) =>{
+//     res.send('You can post to this endpoint...')
+// });
 
 const port = 3000;
 app.listen(port, () => {
