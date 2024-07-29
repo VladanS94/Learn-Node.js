c7fc6d777fb92199e122e9b9c7d87723dd8c92d4
VladanS10 <vladan.nish@gmail.com>
2024-07-29 21:02:25 +0200

    learn mongoose

--- controllers/tourConntroller.js
+++ controllers/tourConntroller.js
@@ -25,6 +25,19 @@ const tours = JSON.parse(
     next();
   }
 
+  exports.getAllTours = (req, res) => {
+    console.log(req.requestTime);
+  
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  };
+
 exports.getTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
